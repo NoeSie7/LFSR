bb29182c6493c901b470d8a2b7a504c99d40b942
NoeSie7 <kukulumbuku@gmail.com>
2019-04-04 13:59:58 +0100

    lEYENDO S-BOX DESDE FICHERO

--- src/main.java
+++ src/main.java
@@ -1,16 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import snow3g.Snow3G;
 
 public class main {
 
+	private static char datos [][];
+	
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        // System.out.println("Holoaaaa");
-        final String hola = "hola";
+        // System.out.println("Holoaaaa"); 
+    	final String hola = "hola";
         // final Lfsr alg = new Lfsr(hola);
         // alg.mostrar();
 
-        final Snow3G alg1 = new Snow3G(hola);
+         final Snow3G alg1 = new Snow3G(hola);
         // alg1.mostrar();
     }
-
+    
+ 
+    
 }
